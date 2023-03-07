de1f7184045939e033813f76f73d32c6a243c0cf
vionaosiako <viona.osiako@student.moringaschool.com>
2023-03-07 22:53:07 +0300

    path: correct the runerror

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,10 @@
 import requests
 
-endpoints = "https://httbbin.org/status/200/"
-endpoints = "https://httbbin.org/"
+# endpoints = "https://httbbin.org/status/200/"
+# endpoints = "https://httbbin.org/"
+endpoints = "http://127.0.0.1:8000/"
 
-get_response = requests.get()#API HTTP Request
+get_response = requests.get(endpoints, json={"query":"Hello world"})#API HTTP Request
 print(get_response.text)#print raw text response
+print(get_response.status_code)
+
