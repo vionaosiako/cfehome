3740dcf0963e26c77b1c27252e7a06cc18efbbc7
vionaosiako <viona.osiako@student.moringaschool.com>
2023-03-07 22:54:40 +0300

    change the endpoint

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 # endpoints = "https://httbbin.org/status/200/"
 # endpoints = "https://httbbin.org/"
-endpoints = "http://127.0.0.1:8000/"
+endpoints = "http://localhost:8000/"
 
 get_response = requests.get(endpoints, json={"query":"Hello world"})#API HTTP Request
 print(get_response.text)#print raw text response
